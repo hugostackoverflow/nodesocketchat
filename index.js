22bd8047b7f2a2d4bfdaa914845132c14c302dc8
Hugo Leite <hugo.leite@glion.edu>
2020-02-17 15:57:13 +0100

    files updated

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const socketio = require('socket.io')(http)
+const csvjson = require('csvjson');
+const readFile = require('fs').readFile;
+const fs = require('fs');
+const allGroups = [];
 
 app.get('/', (req, res) => {
     res.send("Server is running")
 })
 
-app.get('/getfile', function(req, res) {
-    console.log(req.params);
-    var _class = req.params.class;
-    res.send("Testing")
+app.get('/getfile/', function(req, res) {
+    console.log(JSON.stringify(req.query.group));
+    var group = req.query.group;
+  //  var response = readFile2(group);
+    csvToJson(group,res);
+    //res.send("Testing" + JSON.stringify(response))
 })
+
+app.get('/getlist/', function(req, res) {
+
+    listallfiles(res);
+
+    //res.send("Testing" + listallfiles())
+})
+
 
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
@@ -41,6 +55,42 @@ http.listen(app.get('port'),
 });
 
 
-function readFile(file){
+function readFile2(file){
+csvToJson(file)
+ var data = [{id:1,name:"Hugo"},{id:1,name:file}]
+ return data;
+}
 
+function csvToJson(file,res){
+    console.log(file);
+    readFile('./f_schedules/'+file+'.csv', 'utf-8', (err, fileContent) => {
+        if(err) {
+            console.log(err); // Do something to handle the error or just throw it
+            res.send(err)
+            throw new Error(err);
+        }
+        const jsonObj = csvjson.toObject(fileContent);
+        console.log(jsonObj);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(jsonObj)
+    });
+}
+
+function listallfiles(res){
+    const testFolder = './f_schedules/';
+    console.log("#####");
+    const arr = [];
+    fs.readdir(testFolder, function(err, files) {
+      files.forEach(file => {
+         arr.push(file);
+        console.log(file);
+      });
+      res.send(JSON.stringify(arr));
+    });
+}
+
+
+function printFiles(files){
+   // allGroups = files;
+    console.log(files[0]);
 }
